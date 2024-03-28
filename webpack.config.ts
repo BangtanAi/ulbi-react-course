be6e4eef53b17e6e55f8958424d089e940a90451
aidanakairova <a.kairova@codecraft.kz>
2024-03-28 15:08:55 +0500

    lazy, Suspense, добавлен роутинг, общие стили, css modules, useContext, Provider

--- webpack.config.ts
+++ webpack.config.ts
@@ -8,14 +8,14 @@ export default (env: BuildEnv) => {
 
 
     const paths: BuildPaths = {
-        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
         build: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
     }
 
     const mode = env.mode || 'development';
     const isDev = mode === 'development';
-    const PORT = env.port || 3000;
+    const PORT = env.port || 3001;
 
     const config: webpack.Configuration = buildWebpackConfig({
         mode,
